f773a9ade22889725f4e6197b0e8cc6fb85512a7
LIJOMLOYID <lijomloyid@gmail.com>
2021-05-04 17:10:14 +0530

    Added routes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const todo = require("./routes/todo-routes");
 
 const app = express();
 app.use(express.json());
 
 const port = 3000;
+
+app.use("/todo", todo);
 
 app.get("/", (request, response) => {
   console.log("Base route hit!");
---
+++ routes/todo-routes.js
@@ -0,0 +1,10 @@
+// Create tasks
+// Delete tasks
+// Mark task as completed
+// Get all tasks
+
+const express = require("express");
+
+const router = express.Router();
+
+module.exports = router;
