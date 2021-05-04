e931eb1713d94c15b3bfa53e55d55ee4a4ff26ea
LIJOMLOYID <lijomloyid@gmail.com>
2021-05-04 18:42:44 +0530

    Added patch and delete

--- routes/todo-routes.js
+++ routes/todo-routes.js
@@ -13,13 +13,41 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  console.log("Todo Post Route");
   const obj = {
+    id: req.body._id,
     task_name: req.body.task_name,
     date: new Date(),
+    completed: false,
+  };
+  //console.log(obj);
+
+  // Data Processed: Date, Input Validation
+  // Database Logic
+  // Check if task already exists or not.
+
+  res.json({ success: true, msg: "Task successfully added" });
+});
+
+router.patch("/", (req, res) => {
+  console.log("Todo Patch Route");
+
+  const obj = {
+    id: req.body._id,
+    task_name: req.body.task_name,
+    completed: true,
   };
   console.log(obj);
-  res.json({ success: true, msg: "Post Route" });
+
+  res.json({ success: true, msg: "Task Completed" });
+});
+
+router.delete("/", (req, res) => {
+  console.log("Todo Delete Route");
+  const obj = {
+    id: req.body._id,
+  };
+  console.log(obj);
+  res.json({ success: true, msg: "Task Deleted" });
 });
 
 module.exports = router;
