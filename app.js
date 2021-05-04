968c5a8988107d5edbac469a44f76c89d9980aa5
LIJOMLOYID <lijomloyid@gmail.com>
2021-05-04 16:47:07 +0530

    Added base route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 3000;
 
+app.get("/", (request, response) => {
+  console.log("Base route hit!");
+});
+
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
 });
